a1e1542e6d30f157ca2970d907376a1a6fab2af6
Erik Bernhardsson <erikbern@better.com>
2017-12-20 17:24:47 -0500

    fix overloaded variable

--- test_convoys.py
+++ test_convoys.py
@@ -7,8 +7,8 @@ from convoys import Exponential, Gamma
 def test_exponential_model(c=0.05, lambd=0.1, n=1000000):
     # With a really long observation window, the rate should converge to the measured
     C = numpy.array([random.random() < c and scipy.stats.expon.rvs(scale=1.0/lambd) or 0.0 for x in range(n)])
-    N = numpy.array([100 for c in C])
-    B = numpy.array([bool(c > 0) for c in C])
+    N = numpy.array([100 for converted_at in C])
+    B = numpy.array([bool(converted_at > 0) for converted_at in C])
     model = Exponential()
     model.fit(C, N, B)
     assert 0.95*c < model.params['c'] < 1.05*c
@@ -17,8 +17,8 @@ def test_exponential_model(c=0.05, lambd=0.1, n=1000000):
 
 def test_gamma_model(c=0.05, lambd=0.1, k=10.0, n=100000):
     C = numpy.array([random.random() < c and scipy.stats.gamma.rvs(a=k, scale=1.0/lambd) or 0.0 for x in range(n)])
-    N = numpy.array([1000 for c in C])
-    B = numpy.array([bool(c > 0) for c in C])
+    N = numpy.array([1000 for converted_at in C])
+    B = numpy.array([bool(converted_at > 0) for converted_at in C])
     model = Gamma()
     model.fit(C, N, B)
     assert 0.95*c < model.params['c'] < 1.05*c
