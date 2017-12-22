cb9b9ee311d68bbd73a6840b084089483ff09133
Erik Bernhardsson <mail@erikbern.com>
2017-12-22 10:42:37 -0500

    fewer samples for faster tests

--- test_convoys.py
+++ test_convoys.py
@@ -4,7 +4,7 @@ import scipy.stats
 from convoys import Exponential, Gamma, Weibull, Bootstrapper
 
 
-def test_exponential_model(c=0.05, lambd=0.1, n=1000000):
+def test_exponential_model(c=0.05, lambd=0.1, n=100000):
     # With a really long observation window, the rate should converge to the measured
     C = numpy.array([random.random() < c and scipy.stats.expon.rvs(scale=1.0/lambd) or 0.0 for x in range(n)])
     N = numpy.array([100 for converted_at in C])
@@ -16,7 +16,7 @@ def test_exponential_model(c=0.05, lambd=0.1, n=1000000):
     assert 0.95*lambd < model.params['lambd'] < 1.05*lambd
 
 
-def test_gamma_model(c=0.05, lambd=0.1, k=10.0, n=1000000):
+def test_gamma_model(c=0.05, lambd=0.1, k=10.0, n=100000):
     C = numpy.array([random.random() < c and scipy.stats.gamma.rvs(a=k, scale=1.0/lambd) or 0.0 for x in range(n)])
     N = numpy.array([1000 for converted_at in C])
     B = numpy.array([bool(converted_at > 0) for converted_at in C])
@@ -28,7 +28,7 @@ def test_gamma_model(c=0.05, lambd=0.1, k=10.0, n=1000000):
     assert 0.95*k < model.params['k'] < 1.05*k
 
 
-def test_weibull_model(c=0.05, lambd=0.1, k=0.5, n=1000000):
+def test_weibull_model(c=0.05, lambd=0.1, k=0.5, n=100000):
     def sample_weibull():
         # scipy.stats is garbage for this
         # exp(-(x * lambda)^k) = y
