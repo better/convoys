f2106da0d08ee626c89f5c99ae32d8a2cec27c34
Erik Bernhardsson <mail@erikbern.com>
2018-01-22 22:44:13 -0500

    remove old stuff

--- test_convoys.py
+++ test_convoys.py
@@ -7,40 +7,39 @@ matplotlib.use('Agg')  # Needed for matplotlib to run in Travis
 import convoys
 
 
-def test_exponential_model(c=0.3, lambd=0.1, n=100000, cls=convoys.Exponential):
+def test_exponential_model(c=0.3, lambd=0.1, n=100000):
     # With a really long observation window, the rate should converge to the measured
     C = numpy.array([random.random() < c and scipy.stats.expon.rvs(scale=1.0/lambd) or 0.0 for x in range(n)])
     N = numpy.array([100 for converted_at in C])
     B = numpy.array([bool(converted_at > 0) for converted_at in C])
     c = numpy.mean(B)
-    model = cls()
+    model = convoys.ExponentialBeta()
     model.fit(C, N, B)
     assert 0.95*c < model.predict_final() < 1.05*c
     assert 0.95*lambd < model.params['lambd'] < 1.05*lambd
 
+    # Check the confidence intervals
+    y, y_lo, y_hi = model.predict_final(confidence_interval=True)
+    c_lo = scipy.stats.beta.ppf(0.05, n*c, n*(1-c))
+    c_hi = scipy.stats.beta.ppf(0.95, n*c, n*(1-c))
+    assert 0.95*c < y < 1.05 * c
+    assert 0.95*c_lo < y_lo < 1.05 * c_lo
+    assert 0.95*c_hi < y_hi < 1.05 * c_hi
 
-def test_exponential_beta_model():
-    return test_exponential_model(cls=convoys.ExponentialBeta)
 
-
-def test_gamma_model(c=0.3, lambd=0.1, k=3.0, n=100000, cls=convoys.Gamma):
+def test_gamma_model(c=0.3, lambd=0.1, k=3.0, n=100000):
     C = numpy.array([random.random() < c and scipy.stats.gamma.rvs(a=k, scale=1.0/lambd) or 0.0 for x in range(n)])
     N = numpy.array([1000 for converted_at in C])
     B = numpy.array([bool(converted_at > 0) for converted_at in C])
     c = numpy.mean(B)
-    model = cls()
+    model = convoys.GammaBeta()
     model.fit(C, N, B)
     assert 0.95*c < model.predict_final() < 1.05*c
     assert 0.95*lambd < model.params['lambd'] < 1.05*lambd
     assert 0.95*k < model.params['k'] < 1.05*k
 
 
-def test_gamma_beta_model():
-    return test_gamma_model(cls=convoys.GammaBeta)
-
-
-def test_weibull_model(c=0.3, lambd=0.1, k=0.5, n=100000, cls=convoys.Weibull):
-    # TODO: remove this one, only run the WeibullBeta
+def test_weibull_model(c=0.3, lambd=0.1, k=0.5, n=100000):
     def sample_weibull():
         # scipy.stats is garbage for this
         # exp(-(x * lambda)^k) = y
@@ -49,47 +48,11 @@ def test_weibull_model(c=0.3, lambd=0.1, k=0.5, n=100000, cls=convoys.Weibull):
     C = numpy.array([b and sample_weibull() or 1.0 for b in B])
     N = numpy.array([1000 for b in B])
     c = numpy.mean(B)
-    model = cls()
+    model = convoys.WeibullBeta()
     model.fit(C, N, B)
     assert 0.95*c < model.predict_final() < 1.05*c
     assert 0.95*lambd < model.params['lambd'] < 1.05*lambd
     assert 0.95*k < model.params['k'] < 1.05*k
-
-
-def test_weibull_beta_model():
-    return test_weibull_model(cls=convoys.WeibullBeta)
-
-
-def test_bootstrapped_exponential_model(c=0.3, lambd=0.1, n=10000):
-    C = numpy.array([random.random() < c and scipy.stats.expon.rvs(scale=1.0/lambd) or 0.0 for x in range(n)])
-    N = numpy.array([100 for converted_at in C])
-    B = numpy.array([bool(converted_at > 0) for converted_at in C])
-    c = numpy.mean(B)
-    model = convoys.Bootstrapper('exponential')
-    model.fit(C, N, B)
-    y, y_lo, y_hi = model.predict_final(confidence_interval=True)
-    c_lo = scipy.stats.beta.ppf(0.05, n*c, n*(1-c))
-    c_hi = scipy.stats.beta.ppf(0.95, n*c, n*(1-c))
-    assert 0.95*c < y < 1.05 * c
-    assert 0.95*c_lo < y_lo < 1.05 * c_lo
-    assert 0.95*c_hi < y_hi < 1.05 * c_hi
-
-
-def test_exponential_beta_model_2(c=0.3, lambd=0.1, n=10000):
-    C = numpy.array([random.random() < c and scipy.stats.expon.rvs(scale=1.0/lambd) or 0.0 for x in range(n)])
-    N = numpy.array([100 for converted_at in C])
-    B = numpy.array([bool(converted_at > 0) for converted_at in C])
-    c = numpy.mean(B)
-    model = convoys.ExponentialBeta()
-    model.fit(C, N, B)
-    y = model.predict_final()
-    assert 0.95*c < y < 1.05 * c
-    y, y_lo, y_hi = model.predict_final(confidence_interval=True)
-    c_lo = scipy.stats.beta.ppf(0.05, n*c, n*(1-c))
-    c_hi = scipy.stats.beta.ppf(0.95, n*c, n*(1-c))
-    assert 0.95*c < y < 1.05 * c
-    assert 0.95*c_lo < y_lo < 1.05 * c_lo
-    assert 0.95*c_hi < y_hi < 1.05 * c_hi
 
 
 def _get_data(c=0.3, k=10, lambd=0.1, n=1000):
@@ -115,7 +78,3 @@ def test_plot_cohorts():
 
 def test_plot_conversion():
     convoys.plot_timeseries(_get_data(), window=datetime.timedelta(days=7), model='gamma')
-
-
-def test_plot_cohorts_share_params():
-    convoys.plot_cohorts(_get_data(), projection='gamma', share_params=['k'])
