f1145ab2b722b40b3e5e89f3d99f252c005af453
Erik Bernhardsson <erikbern@better.com>
2018-03-20 14:21:54 -0400

    remove kaplan-meier

--- convoys/single.py
+++ convoys/single.py
@@ -1,5 +1,4 @@
 import bisect
-import lifelines
 import numpy
 from scipy.special import expit
 import tensorflow as tf
@@ -8,42 +7,6 @@ from convoys import tf_utils
 
 class SingleModel:
     pass  # TODO
-
-
-class KaplanMeier(SingleModel):
-    def fit(self, B, T):
-        kmf = lifelines.KaplanMeierFitter()
-        kmf.fit(T, event_observed=B)
-        self.ts = kmf.survival_function_.index.values
-        self.ps = 1.0 - kmf.survival_function_['KM_estimate'].values
-        self.ps_hi = 1.0 - kmf.confidence_interval_['KM_estimate_lower_0.95'].values
-        self.ps_lo = 1.0 - kmf.confidence_interval_['KM_estimate_upper_0.95'].values
-
-    def predict(self, ts, ci=None):
-        # TODO: should also handle scalars
-        js = [bisect.bisect_left(self.ts, t) for t in ts]
-        def array_lookup(a):
-            return numpy.array([a[min(j, len(self.ts)-1)] for j in js])
-        if ci is not None:
-            return (array_lookup(self.ps), array_lookup(self.ps_lo), array_lookup(self.ps_hi))
-        else:
-            return array_lookup(self.ps)
-
-    def predict_final(self, ci=None):
-        if ci is not None:
-            return (self.ps[-1], self.ps_lo[-1], self.ps_hi[-1])
-        else:
-            return self.ps[-1]
-
-    def predict_time(self, ci=None):
-        # TODO: should not use median here, but mean is no good
-        def median(ps):
-            i = bisect.bisect_left(ps, 0.5)
-            return self.ts[min(i, len(ps)-1)]
-        if ci is not None:
-            return median(self.ps), median(self.ps_lo), median(self.ps_hi)
-        else:
-            return median(self.ps)
 
 
 class Nonparametric(SingleModel):
