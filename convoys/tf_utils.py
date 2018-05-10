80fa0f94f75709d6f332292af692146f4c197e50
Erik Bernhardsson <erikbern@better.com>
2018-05-10 12:59:35 -0400

    better output

--- convoys/tf_utils.py
+++ convoys/tf_utils.py
@@ -15,7 +15,7 @@ def optimize(sess, target, update_callback=None):
             update_callback(sess)
 
     def loss_callback(loss):
-        sys.stderr.write('Current loss: %30.3f\r' % loss)
+        sys.stderr.write('Current loss: %9.6e\r' % loss)
 
     optimizer = ScipyOptimizerInterface(loss=-target,
                                         method='SLSQP',
