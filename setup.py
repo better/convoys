902254267da8ea00d262cf0e9a9b419f7b5e0b61
Erik Bernhardsson <erikbern@better.com>
2018-12-04 14:04:00 -0500

    link from pypi to github and docs

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from setuptools import setup, find_packages
 
+long_description = '''
+For more information, see
+`the package documentation <https://better.engineering/convoys>`_
+or
+`the Github project page <https://github.com/better/convoys>`_.
+
+.. image:: https://better.engineering/convoys/_images/dob-violations-combined.png
+'''
+
 setup(name='convoys',
-      version='0.1.0',
+      version='0.1.2',
+      description='Fit machine learning models to predict conversion using Weibull and Gamma distributions',
+      long_description=long_description,
+      url='https://better.engineering/convoys',
+      license='MIT',
+      author='Erik Bernhardsson',
+      author_email='erikbern@better.com',
       packages=find_packages(),
       install_requires=[line.strip() for line in open('requirements.txt')])
