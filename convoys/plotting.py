d5f9b0928f2ec14bafc455a94f80eaf9638123f3
stphnma <stphnma@gmail.com>
2018-12-11 18:13:56 -0500

    hound test

--- convoys/plotting.py
+++ convoys/plotting.py
@@ -29,13 +29,13 @@ def plot_cohorts(G, B, T, t_max=None, model='kaplan-meier',
     if t_max is None:
         _, t_max = pyplot.gca().get_xlim()
         t_max = max(t_max, max(T))
-
-    if not isinstance(model, convoys.multi.MultiModel):
-        # Fit model
-        m = _models[model](ci=bool(ci))
-        m.fit(G, B, T)
-    else:
-        m = model
+    
+    # if not isinstance(model, convoys.multi.MultiModel):
+    #     # Fit model
+    #     m = _models[model](ci=bool(ci))
+    #     m.fit(G, B, T)
+    # else:
+    #     m = model
 
     if specific_groups is None:
         specific_groups = groups
