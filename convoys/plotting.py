1c500e828a4e026b30b9f92f7cde20c35f58c90a
stphnma <stphnma@gmail.com>
2018-12-11 17:29:52 -0500

    allowing G, B, and T to be optional

--- convoys/plotting.py
+++ convoys/plotting.py
@@ -14,20 +14,27 @@ _models = {
 }
 
 
-def plot_cohorts(G, B, T, t_max=None, model='kaplan-meier',
+def plot_cohorts(G=None, B=None, T=None, t_max=None, model='kaplan-meier',
                  ci=None, plot_kwargs={}, plot_ci_kwargs={},
                  groups=None, specific_groups=None):
-          
+    
+    if model not in _models.keys():
+        if not isinstance(model, convoys.multi.MultiModel)):
+            raise Exception('model must be of %s or a convoys object' % str(_models.keys()))
+
+    if model in _models.keys():
+        if G is None or B is None or T is None:
+            raise Exception('G, B, T must be supplied to fit model!')
+        if groups is None:
+            groups = list(set(G))
+
+    if isinstance(model, convoys.multi.MultiModel) and groups is None:
+        raise Exception('groups must be supplied with trained model!')
+ 
     # Set x scale
     if t_max is None:
         _, t_max = pyplot.gca().get_xlim()
         t_max = max(t_max, max(T))
-
-    if groups is None:
-        groups = list(set(G))
-    if model not in _models.keys():
-        if not isinstance(model, convoys.multi.MultiModel)):
-            raise Exception('model must be of %s or a convoys object' % str(_models.keys()))
 
     if not isinstance(model, convoys.multi.MultiModel):
         # Fit model
