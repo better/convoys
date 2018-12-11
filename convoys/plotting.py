eefd47d7bd2841ffe18cec1660c1b43ae51be572
stphnma <stphnma@gmail.com>
2018-12-11 16:58:48 -0500

    hound

--- convoys/plotting.py
+++ convoys/plotting.py
@@ -15,7 +15,9 @@ _models = {
 
 
 def plot_cohorts(G, B, T, t_max=None, model='kaplan-meier',
-                 ci=None, plot_kwargs={}, plot_ci_kwargs={}, groups=None, specific_groups=None):
+                 ci=None, plot_kwargs={}, plot_ci_kwargs={}, 
+                 groups=None, specific_groups=None):
+                 
     # Set x scale
     if t_max is None:
         _, t_max = pyplot.gca().get_xlim()
