d0c9e592d1c3f0b78e2cd42e8a22ad4b217e0c88
Sophie Zhao <sophie.zhao@evenfinancial.com>
2020-10-08 10:41:06 -0400

    fixed individual functions and they work

--- convoys/regression.py
+++ convoys/regression.py
@@ -3,7 +3,7 @@ import autograd
 from autograd_gamma import gammainc
 from deprecated.sphinx import deprecated
 import emcee
-import numpy
+from autograd import numpy
 from scipy.special import gammaincinv
 from autograd.scipy.special import expit, gammaln
 from autograd.numpy import isnan, exp, dot, log, sum, array, repeat
@@ -47,10 +47,10 @@ def generalized_gamma_loss(x, X, B, T, W, fix_k, fix_p,
         c = dot(X, beta)+b
         LL_observed = -(1 - c)**2 + log_pdf
         LL_censored = -(c*cdf)**2
-
-    LL_data = sum(
+    print("shape of result before sum", (W * B * LL_observed + W * (1 - B) * LL_censored).shape)
+    LL_data = numpy.sum(
         W * B * LL_observed +
-        W * (1 - B) * LL_censored, 0)
+        W * (1 - B) * LL_censored, 0)   #
     if hierarchical:
         # Hierarchical model with sigmas ~ invgamma(1, 1)
         LL_prior_a = -4*log_sigma_alpha - 1/exp(log_sigma_alpha)**2 \
@@ -85,10 +85,10 @@ def double_hierarchy_weibull_loss(x, X, B, T, W, fix_k, fix_p,
     a = x[6]
     b = x[7]
     alpha_1 = x[8:8+n_group1] # length=n_group1
-    beta_1 = x[8+num_group_1:8+2*n_group1] # length=n_group1
+    beta_1 = x[8+n_group1:8+2*n_group1] # length=n_group1
     n_group=n_group1*n_group2
     alpha_2 = x[8+2*n_group1:8+2*n_group1+n_group].reshape(n_group1,n_group2) # length = n_group1*n_group2
-    bete_2 = x[8+2*n_group1+n_group:8+2*n_group1+2*n_group].reshape(n_group1,n_group2) # length = n_group1*n_group2
+    beta_2 = x[8+2*n_group1+n_group:8+2*n_group1+2*n_group].reshape(n_group1,n_group2) # length = n_group1*n_group2
 
     # If X is (N, G1,G2), alpha_2 is (G1,G2), then X2 *alpha_2 is (N, G1,G2), call this Z_2
     lambd = exp(numpy.sum(X * alpha_2 , axis=(1,2))+a)
@@ -102,10 +102,8 @@ def double_hierarchy_weibull_loss(x, X, B, T, W, fix_k, fix_p,
     LL_observed = log(c) + log_pdf
     LL_censored = log((1 - c) + c * (1 - cdf))
   
-    # W should be sample weights, it is not implemented in this project 
-    LL_data = sum(
-        W * B * LL_observed +
-        W * (1 - B) * LL_censored, 0)
+    # W should be sample weights, it is not implemented right now, let's delete it 
+    LL_data = sum(B * LL_observed +(1 - B) * LL_censored)
     #TODO: rewrite this loss
     if hierarchical:
         # Hierarchical model with sigmas ~ invgamma(1, 1)
@@ -113,21 +111,20 @@ def double_hierarchy_weibull_loss(x, X, B, T, W, fix_k, fix_p,
         # alpha_1 is (G1,) -> make this (n_group1,1)
         # diff_alpha = alpha_2 - alpha_1 -> this is therefore (n_group1,n_group2)
         
-        diff_alpha_sq = numpy.sum((alpha_2-alpha_1)**2)
-        diff_beta = numpy.sum((beta_2-beta_1)**2)
+        diff_alpha_sq = numpy.sum((alpha_2-numpy.expand_dims(alpha_1,axis=-1))**2)
+        diff_beta_sq = numpy.sum((beta_2-numpy.expand_dims(beta_1,axis=-1))**2)
      
 
         LL_prior_a = -4*log_sigma_alpha_1 - 1/exp(log_sigma_alpha_1)**2 \
                      - dot(alpha_1, alpha_1) / (2*exp(log_sigma_alpha_1)**2) \
                      - n_group1*log_sigma_alpha_1 - \
-                     diff_alpha_sq/(2*exp(log_sigma_2)**2)
+                     diff_alpha_sq/(2*exp(log_sigma_alpha_2)**2) \
                      -n_group*log_sigma_alpha_2
 
         LL_prior_b = -4*log_sigma_beta_1 - 1/exp(log_sigma_beta_1)**2 \
                      - dot(beta_1, beta_1) / (2*exp(log_sigma_beta_1)**2) \
                      - n_group1*log_sigma_beta_1 - \
-                     diff_beta_sq/(2*exp(log_beta_2)**2)
-                     -n_group*log_sigma_beta_2
+                     diff_beta_sq/(2*exp(log_sigma_beta_2)**2) -n_group*log_sigma_beta_2
 
         LL = LL_prior_a + LL_prior_b + LL_data
     else:
